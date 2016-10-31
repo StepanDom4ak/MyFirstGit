da519aec9fee714fb0cc5b7ec2ef33c1322658f1
StepanDom4ak <stepandom4ak@gmail.com>
2016-10-31 13:08:42 +0200

    NewGit

---
+++ Fish.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGit
+{
+    class Fish
+    {
+        string Name { set; get; }
+        double Weight { set; get; }
+        double Price { set; get; }
+
+        public override String ToString()
+        {
+            return String.Format("Name: {0}, Weight: {1}, Price: {2}", Name, Weight, Price);
+        }
+    }
+}
